aac053f5fe01cf57a2cfffe1e8eee784a1d68e89
Uptownboys32 <anggaalfian133@gmail.com>
2021-11-05 16:12:43 +0700

    citrus: Enable BUILD_BROKEN_DUP_RULES

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,8 @@ include device/xiaomi/sm6115-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/citrus
 
+BUILD_BROKEN_DUP_RULES := true
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
